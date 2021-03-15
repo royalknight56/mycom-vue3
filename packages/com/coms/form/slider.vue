29803e3ee07c128d4c3b4f39a07d04bc0efc7134
royalknight56 <34654733+royalknight56@users.noreply.github.com>
2021-03-15 22:36:32 +0800

    1.0315
    alert改变代码结构,
    slider优化滑动到两端逻辑

--- packages/com/coms/form/slider.vue
+++ packages/com/coms/form/slider.vue
@@ -4,12 +4,16 @@
  * @Author: RoyalKnight
  * @Date: 2020-10-01 15:09:52
  * @LastEditors: RoyalKnight
- * @LastEditTime: 2020-10-07 21:00:41
+ * @LastEditTime: 2021-03-15 17:57:47
 -->
 <template>
   <div draggable="false" class="mc_slider">
-      <div :class="ifdrug?'druging':'nodrug'" draggable="false" ref='button' class="mc_slider_button">
-      </div>
+    <div
+      :class="ifdrug ? 'druging' : 'nodrug'"
+      draggable="false"
+      ref="button"
+      class="mc_slider_button"
+    ></div>
   </div>
 </template>
 
@@ -19,139 +23,149 @@ export default {
   name: "mc-slider",
   mixins: [setting],
   // props:['value','top'],
-  props:{
-    value:{
-      type:Number
+  props: {
+    value: {
+      type: Number,
     },
-    top:{
-      default:100,
-      type:Number
-    }
+    top: {
+      default: 100,
+      type: Number,
+    },
   },
   data: function () {
     return {
       ifLocked: false,
-      num:0,
-      ifdrug:false,
-      bleft : 10,
-      bright:270,
+      num: 0,
+      ifdrug: false,
+      bleft: 10,
+      bright: 280,
     };
   },
   emits: {
-    ['update:value']: () => {
+    ["update:value"]: () => {
       return true;
     },
   },
-  watch:{
-    value:function(){
-      this.$refs.button.style.left=this.value/this.top*(this.bright-this.bleft-1)+this.bleft+'px'
-    }
+  watch: {
+    value: function () {
+      this.$refs.button.style.left =
+        (this.value / this.top) * (this.bright - this.bleft - 1) +
+        this.bleft +
+        "px";
+    },
   },
   mounted: function () {
-    this.$refs.button.style.left=this.value/this.top*(this.bright-this.bleft-1)+this.bleft+'px'
-      var left = this.bleft;
-      var right= this.bright;
-      var $this=this
-      var start={
-          x:0,
-          buttonx:0,
+    this.$refs.button.style.left =
+      (this.value / this.top) * (this.bright - this.bleft - 1) +
+      this.bleft +
+      "px";
+    var left = this.bleft;
+    var right = this.bright;
+    var $this = this;
+    var start = {
+      x: 0,
+      buttonx: 0,
+    };
+    this.$refs.button.addEventListener("touchstart", function (e) {
+      start.x = e.touches[0].pageX;
+      start.buttonx = $this.$refs.button.style.left.slice(0, -2);
+      $this.ifdrug = true;
+    });
+    document.addEventListener("touchmove", function (e) {
+      if ($this.ifdrug) {
+        let temp = start.buttonx - start.x + e.touches[0].pageX;
+        if (temp > right) {
+          temp = 280;
+        } else if (temp < left) {
+          temp = 10;
+        }
+        $this.$refs.button.style.left = temp + "px";
+        $this.num = parseInt((100 * (temp - left)) / (right - left - 1));
+        $this.emit($this.num);
       }
-      this.$refs.button.addEventListener('touchstart',function(e){
-          start.x=e.touches[0].pageX;
-          start.buttonx=$this.$refs.button.style.left.slice(0,-2);
-          $this.ifdrug=true;
-      })
-      document.addEventListener('touchmove',function(e){
-          if($this.ifdrug){
-              var temp=start.buttonx-start.x+e.touches[0].pageX;
-              if(temp<left||temp>right){
-                  //
-              }else{
-                $this.$refs.button.style.left=start.buttonx-start.x+e.touches[0].pageX+'px'
-                $this.num=parseInt(100*(temp-left)/(right-left-1));
-                $this.emit($this.num)
-              }
-            }
-      })
-      document.addEventListener('touchend',function(){
-          $this.ifdrug=false;
-      })
+    });
+    document.addEventListener("touchend", function () {
+      $this.ifdrug = false;
+    });
 
-      this.$refs.button.addEventListener('mousedown',function(e){
-          start.x=e.pageX;
-          start.buttonx=$this.$refs.button.style.left.slice(0,-2);
-          $this.ifdrug=true;
-      })
-      document.addEventListener('mousemove',function(e){
-          if($this.ifdrug){
-              var temp=start.buttonx-start.x+e.pageX;
-              if(temp<=left||temp>=right){
-                  //
-              }else{
-                $this.$refs.button.style.left=start.buttonx-start.x+e.pageX+'px'
-                $this.num=parseInt(100*(temp-left)/(right-left-1));
-                $this.emit($this.num)
-              }
-            }
-      })
-      document.addEventListener('mouseup',function(){
-          $this.ifdrug=false;
-      })
+    this.$refs.button.addEventListener("mousedown", function (e) {
+      start.x = e.pageX;
+      start.buttonx = $this.$refs.button.style.left.slice(0, -2);
+      $this.ifdrug = true;
+    });
+    document.addEventListener("mousemove", function (e) {
+      if ($this.ifdrug) {
+        let temp = start.buttonx - start.x + e.pageX;
 
+        if (temp > right) {
+          temp = 280;
+        } else if (temp < left) {
+          temp = 10;
+        }
+        $this.$refs.button.style.left = temp + "px";
+        $this.num = parseInt((100 * (temp - left)) / (right - left - 1));
+        $this.emit($this.num);
+
+
+      }
+    });
+    document.addEventListener("mouseup", function () {
+      $this.ifdrug = false;
+    });
   },
-  methods:{
-    emit:function(num){
+  methods: {
+    emit: function (num) {
       // this.num=parseFloat(100*(temp-4)/266);
-      if(this.top){
-        num=num/(100*(1/this.top))
-      }else{
+      if (this.top) {
+        num = num / (100 * (1 / this.top));
+      } else {
         //没有定义top
       }
-      
-      this.$emit('update:value',num)
-    }
-  }
+
+      this.$emit("update:value", num);
+    },
+  },
 };
 </script>
 <style lang="scss" scoped>
-@import  '../../../scssvar.scss';
+@import "../../../scssvar.scss";
 
-.mc_slider{
-    position: relative;
-    margin: 4px 0;
-    width: 300px;
-    height: 30px;
-    top: 50%;
-    background-color: rgba(49, 49, 49, 0.747);
-    // border-radius: 15px;
-    // border: 1px solid rgba(0, 0, 0, 0.247);
-    position: relative;
-    transition: all 0.3s;
-    cursor: pointer;
+.mc_slider {
+  position: relative;
+  margin: 4px 0;
+  width: 300px;
+  height: 20px;
+  top: 50%;
+  background-color: rgba(49, 49, 49, 0.747);
+  // border-radius: 15px;
+  // border: 1px solid rgba(0, 0, 0, 0.247);
+  position: relative;
+  transition: all 0.3s;
+  cursor: pointer;
 }
-.mc_slider:hover{
-    background-color:rgba(63, 63, 63, 0.747);
-    height: 30px;
+.mc_slider:hover {
+  background-color: rgba(63, 63, 63, 0.747);
+  height: 20px;
 }
 
-.mc_slider_button{
-    position: absolute;
-    left: 10px;
-    top: 4px;
+.mc_slider_button {
+  position: absolute;
+  left: 10px;
+  top: 4px;
 
-    width: 20px;
-    height: 20px;
-    background-color: rgb(238, 238, 238);
-    transform: scale(0.8) ;
-    transition: transform 0.1s;
-    user-select: none;
+  width: 12px;
+  height: 12px;
+  background-color: rgb(238, 238, 238);
+  transform: scale(0.8);
+  transition: transform 0.1s;
+  user-select: none;
 }
-.druging{
+.druging {
   transform: scale(1.05);
 }
-.mc_slider_button:hover{
-    background-color: rgb(255, 255, 255);
-    transform: scale(1.1);
+.mc_slider_button:hover {
+  background-color: rgb(255, 255, 255);
+  transform: scale(1.1);
 }
 /* .mc_switch_cir_left{
     left: 4px;
--- packages/jsApi.js
+++ packages/jsApi.js
@@ -4,7 +4,7 @@
  * @Author: RoyalKnight
  * @Date: 2021-01-16 17:33:38
  * @LastEditors: RoyalKnight
- * @LastEditTime: 2021-01-16 17:46:07
+ * @LastEditTime: 2021-03-15 16:29:35
  */
 import alert_basic from './com/coms/alert/alert_basic.vue'
 import top_alert_vue from './com/coms/alert/top_alert_vue.vue'
@@ -12,53 +12,36 @@ import right_alert_vue from './com/coms/alert/right_alert_vue.vue'
 import modal_alert_vue from './com/coms/alert/modaldialog_vue.vue'
 import { reactive, createApp } from 'vue'
 
-export let alert_app = reactive(null)
-export let top_alert_app = reactive(null)
-export let right_alert_app = reactive(null)
-export let modal_alert_app = reactive(null)
+export let alert_app = reactive({value:null})
+export let top_alert_app = reactive({value:null})
+export let right_alert_app = reactive({value:null})
+export let modal_alert_app = reactive({value:null})
+
+
+function ifMontedAndAlert(app,com,id,...par){
+    if (app.value) {
+        //
+    } else {
+        var div = document.createElement('div');
+        div.id = id;
+        document.body.appendChild(div);
+        app.value = createApp(com).mount('#'+id)
+    }
+    app.value.show(...par)
+}
+
 
 export function alert(text) {
-    if (alert_app) {
-        //
-    } else {
-        var div = document.createElement('div');
-        div.id = 'mcalert';
-        document.body.appendChild(div);
-        alert_app = createApp(alert_basic).mount('#mcalert')
-    }
-    alert_app.show(text)
+    ifMontedAndAlert(alert_app,alert_basic,'mcalert',text)
 }
 export function top_alert(text, timeout) {
-    if (top_alert_app) {
-        //
-    } else {
-        var div = document.createElement('div');
-        div.id = 'mctopalert';
-        document.body.appendChild(div);
-        top_alert_app = createApp(top_alert_vue).mount('#mctopalert')
-    }
-    top_alert_app.show(text, timeout)
+    ifMontedAndAlert(top_alert_app,top_alert_vue,'mctopalert',text, timeout)
 }
+
 export function right_alert(text, timeout) {
-    if (right_alert_app) {
-        //
-    } else {
-        var div = document.createElement('div');
-        div.id = 'mcrightalert';
-        document.body.appendChild(div);
-        right_alert_app = createApp(right_alert_vue).mount('#mcrightalert')
-    }
-    right_alert_app.show(text, timeout)
+    ifMontedAndAlert(right_alert_app,right_alert_vue,'mcrightalert',text, timeout)
 }
 export function modal_alert(text, res, rej) {
-    if (modal_alert_app) {
-        //
-    } else {
-        var div = document.createElement('div');
-        div.id = 'mcmodalalert';
-        document.body.appendChild(div);
-        modal_alert_app = createApp(modal_alert_vue).mount('#mcmodalalert')
-    }
-    modal_alert_app.show(text, res, rej)
+    ifMontedAndAlert(modal_alert_app,modal_alert_vue,'mcmodalalert',text, res, rej)
 }
 
